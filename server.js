95c404aa0f81e5c3707a631543ae99bd8a4584e9
Martin Wolky <m.wolky@gmail.com>
2016-10-23 21:10:13 +0200

    fixing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 app.use(function(req,res,next){
-  if(req.header['X-Forwarded-Proto'] === 'http'){
+  if(req.header['X-Forwarded-Proto'] === 'https'){
     next();
   } else {
     res.redirect('http://'+ req.hostname + req.url);
