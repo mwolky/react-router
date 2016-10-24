ad15f21035b10bd9dbeb82e27dcf1999f4d97511
Martin Wolky <m.wolky@gmail.com>
2016-10-24 18:58:49 +0200

    Styles nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,44 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props)=>{
+var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert("To do");
+  },
+  render: function(){
     return (
-      <div>
-        <IndexLink to="/" activeClassName="active" activeStyle={{color: 'red'}}>Get weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{color: 'red'}}>About</Link>
-        <Link to="/example" activeClassName="active" activeStyle={{color: 'red'}}>Example</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Weather App</li>
+            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+            <li><Link to="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="text" placeholder="Search..."/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get weather"/>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
-
     );
-  };
+  }
+});
 
 module.exports = Nav;
+var old = (
+  <div>
+    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get weather</IndexLink>
+    <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+    <Link to="/example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
+  </div>
+);
